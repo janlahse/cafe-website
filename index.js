82f29fe8e0b0522c2131b2c4c54f758ae979afc1
Jan <jan.lahse@gmx.de>
2025-11-26 12:01:32 +0100

    fixed burger menu

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const burgerButton = document.querySelector(".burger-button");
 const burgerContent = document.querySelector(".burger-content");
+const burgerIconOuter = document.querySelector(".burger-icon-outer");
 
 burgerButton.addEventListener("click", () => {
   burgerContent.classList.toggle("burger-content-active");
-  burgerButton.classList.toggle("burger-button-active");
+  burgerIconOuter.classList.toggle("burger-icon-outer-hide");
 });
 
 document.addEventListener("click", (e) => {
   if (!burgerButton.contains(e.target) && !burgerContent.contains(e.target)) {
     burgerContent.classList.remove("burger-content-active");
-    burgerButton.classList.remove("burger-button-active");
+    burgerIconOuter.classList.remove("burger-icon-outer-hide");
   }
 });
