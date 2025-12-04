fbb798c578998cbcfdd534c5d5a6e050d4b69643
Jan <jan.lahse@gmx.de>
2025-12-04 13:33:00 +0100

    added menu

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const burgerButton = document.querySelector(".burger-button");
 const burgerContent = document.querySelector(".burger-content");
 const burgerIconOuter = document.querySelector(".burger-icon-outer");
+const openMenuButton = document.querySelector(".open-menu-button");
+const closeMenuButton = document.querySelector(".close-menu-button");
+const menu = document.querySelector(".menu");
+const menuCard = document.querySelector(".menu-card");
 
 burgerButton.addEventListener("click", () => {
   burgerContent.classList.toggle("burger-content-active");
@@ -11,5 +15,19 @@ document.addEventListener("click", (e) => {
   if (!burgerButton.contains(e.target) && !burgerContent.contains(e.target)) {
     burgerContent.classList.remove("burger-content-active");
     burgerIconOuter.classList.remove("burger-icon-outer-hide");
+  }
+});
+
+openMenuButton.addEventListener("click", () => {
+  menu.classList.add("menu-active");
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    (!menuCard.contains(e.target) && !openMenuButton.contains(e.target)) ||
+    closeMenuButton.contains(e.target)
+  ) {
+    menu.classList.remove("menu-active");
+    menu.scroll({ top: 0, behavior: "smooth" });
   }
 });
